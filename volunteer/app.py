4d40e517a37957f540fc4f729407cde0d60e617d
Varun Ved <varunmved@gmail.com>
2016-04-08 22:12:42 -0700

    adding my basic eventbrite API code

---
+++ volunteer/app.py
@@ -0,0 +1,19 @@
+from eventbrite import Eventbrite
+
+
+eventbrite = Eventbrite('7C3O54QSC636WO5OJVJK')
+
+def test():
+    user = eventbrite.get_user()
+    name = user['name']
+    print(name)
+
+def create_event():
+    first_event = {"event.name.html":"my_first_event","event.start.utc":"2017-01-31T13:00:00z",
+            "event.start.timezone":"America/Los_Angeles","event.end.utc":"2017-01-31T14:00:00z",
+            "event.end.timezone":"America/Los_Angeles","event.currency":"USD"
+    }
+    event = eventbrite.post_event(first_event)
+    print(event)
+
+create_event()
